94d542d92aa668983622088df21149d14a898179
wahid <wahidahmed121992@gmail.com>
2025-08-12 17:38:26 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace demoCICD_Docker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
